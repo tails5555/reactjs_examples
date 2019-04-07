82d7809687c21495d2f383981980680a00279ec9
tails5555 <tails5555@naver.com>
2019-04-07 17:08:26 +0900

    Create Music REST API Done 간단한 음악 목록을 보여주는 REST API 생성 완료.

--- api_apps/music_app/songbox/models.py
+++ api_apps/music_app/songbox/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model) :
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20, null=False, default='')
+
+class Music(models.Model) :
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, null=False, default='')
+    singer = models.CharField(max_length=100, null=False, default='')
+    year = models.IntegerField(null=False, default=1900)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    hearts = models.IntegerField(null=False, default=0)
