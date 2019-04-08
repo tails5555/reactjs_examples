35c25ef1f059f5cf398fbaac7fd6bf6d6f9b059c
tails5555 <tails5555@naver.com>
2019-04-08 15:01:03 +0900

    Create CheckButton, InputProperty Component 데이터 수정에 필요한 Component 작성 중.

--- simple_crud/src/components/music/Element.js
+++ simple_crud/src/components/music/Element.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
-const Element = ({ music }) => (
+const CheckButton = ({ checked, act_edit, act_cancel }) => 
+    !checked ?
+        <button onClick={ act_edit } className="btn btn-info">
+            <i className="fas fa-edit" /> 편집
+        </button> :
+        <button onClick={ act_cancel } className="btn btn-danger">
+            <i className="fas fa-times" /> 취소
+        </button>
+
+const InputProperty = ({ name, value, act_change }) => 
+    <input type="text" className="form-control" name={ name } value={ value } onChange={ act_change } />
+
+const Element = ({ music, changeAction, editAction, cancelAction }) => (
     <tr>
         <td>{ music.id }</td>
-        <td>{ music.title }</td>
+        <td>{ !music.checked ? music.title : <InputProperty name="title" value={ music.title } act_change={ changeAction } /> }</td>
         <td>{ music.singer }</td>
         <td>{ music.year }</td>
         <td>{ music.genre && music.genre.name }</td>
-        <td>버튼</td>
+        <td onClick={ !music.checked ? editAction : cancelAction }>
+            <CheckButton checked={music.checked} editAction={editAction} cancelAction={cancelAction} />
+        </td>
     </tr>
 );
 
