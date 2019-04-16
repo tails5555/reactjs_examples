85f694861bccef46e4ef778e620c757296d7139b
tails5555 <tails5555@naver.com>
2019-04-17 01:19:16 +0900

    Create BBS Model Class BBS API 에서 사용할 Model 데이터베이스 생성 완료.

--- api_apps/bbs_app/bbs_rest/models.py
+++ api_apps/bbs_app/bbs_rest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model) :
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20, null=False, default='')
+
+class Post(models.Model) :
+    id = models.AutoField(primary_key=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    writer = models.CharField(max_length=20, null=False, default='')
+    title = models.CharField(max_length=100, null=False, default='')
+    context = models.TextField(null=False, default='')
+    views = models.IntegerField(null=False, default=0)
+
+class Comment(models.Model) :
+    id = models.AutoField(primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    writer = models.CharField(max_length=20, null=False, default='')
+    context = models.TextField(null=False, default='')
--- api_apps/music_app/music_app/settings.py
+++ api_apps/music_app/music_app/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'music_app.urls'
